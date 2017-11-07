36abb0b273618b1161305ece237a4e0c4a267c06
jordaninthewind <jordan.kline@gmail.com>
2017-11-07 19:25:43 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -7,12 +7,12 @@
 # num = gets.chomp
 
 def fizzbuzz(num)
-  if (num % 3 == 0 && num % 5 == 0)
-    return 'FizzBuzz'
-  elsif (num % 3 == 0)
-    return 'Fizz'
-  elsif (num % 5 == 0)
-    return 'Buzz'
-  else
-    return nil
+  # if (num % 3 == 0 && num % 5 == 0)
+  #   return 'FizzBuzz'
+  # elsif (num % 3 == 0)
+  #   return 'Fizz'
+  # elsif (num % 5 == 0)
+  #   return 'Buzz'
+  # else
+  #   return nil
 end
